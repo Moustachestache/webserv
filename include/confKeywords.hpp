7f6eae127b8971ee1137bd857e8be3e11238425a
gbricot <gbricot@student.42perpignan.fr>
2024-05-12 11:22:09 +0200

    Added confKeywoprds.hpp

---
+++ include/confKeywords.hpp
@@ -0,0 +1,21 @@
+
+char	*SERVER_KW[] = {
+							"SERVER_NAME",
+							"ROOT",
+							"REQUEST_SIZE",
+							"ROUTE",
+							"ERROR_STATUS",
+							"ERROR_LOG"		};
+
+char	*ROUTE_KW[] = {
+							"PATH",
+							"METHOD",
+							"REDIRECTION",
+							"LISTING",
+							"DEFAULT",
+							"ALLOW_UPLOAD",
+							"UPLOAD_PATH",
+							"SESSION_TIMEOUT",
+							"CGI"
+											};
+
