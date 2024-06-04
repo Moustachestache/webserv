c19b2b8278ad0e303741968fae1ecc2a9cbc06d0
yoshi <maouvrar@student.42perpignan.fr>
2024-06-04 16:14:33 +0200

    modif CGI

--- include/Cgi/Cgi.cpp
+++ include/Cgi/Cgi.cpp
@@ -68,42 +68,6 @@ std::string execCgi(HttpHeader _header)
 	return (0);
 }
 
-/*void execCgiPost(HttpHeader _header)
-{
-	//executé le fichier sh via la http request
-	int pipe_fd[2];
-	pid_t pid;
-
-	pipe(pipe_fd);
-	dup2(STDOUT_FILENO, pipe_fd[1]);
-	pid = fork();
-
-	if (pid == -1)
-	{
-		std::cerr << "fork failed" << std::endl;
-		exit(1);
-	}
-	else if (pid == 0)
-	{
-		//on execute le cgi
-		char **argv = make_arg(_header);
-		execve(_header.getFile().c_str(), argv, NULL);
-		std::cerr << "execve failed" << std::endl;
-		exit(0);
-	}
-	else
-	{
-		waitpid(pid, NULL, 0);
-		//on recupere le resultat
-		char buffer[1024];
-		int ret;
-		while ((ret = read(pipe_fd[0], buffer, 1024)) > 0)
-		{
-			write(STDOUT_FILENO, buffer, ret);
-		}
-	}
-}*/
-
 std::string execCgiGet(HttpHeader _header)
 {
 	int pipe_fd[2];
@@ -132,7 +96,7 @@ std::string execCgiGet(HttpHeader _header)
 		
 		
 		std::string file = _header.getFile().insert(0, "./www");
-		char *script_path = const_cast<char *>(file.c_str());
+		char *script_path = const_cast<char *>(file.c_str());²
 		char *argv[] = { script_path, NULL };
 		char *envp[] = { NULL };
 
@@ -155,8 +119,6 @@ std::string execCgiGet(HttpHeader _header)
 			answer.append(buffer, ret);
 		}
 		close(pipe_fd[0]);
-		
-		std::cout << "Aigue Marine 2 " << answer << "Fin Aigue Marine " <<std::endl ;
 		
 		return (answer);
 	}
