910ce901c87e8a982b07aeb47e2c3b654b05070b
yoshi <maouvrar@student.42perpignan.fr>
2024-06-05 16:23:08 +0200

    repparation de CGI

--- include/Cgi/Cgi.cpp
+++ include/Cgi/Cgi.cpp
@@ -13,6 +13,36 @@
 	return arg;
 }*/
 
+
+
+std::string cgiPath(std::vector<Route> routes, HttpHeader header)
+{
+	std::string file_verif;
+	for (std::vector<Route>::iterator it = route.begin(); it != route.end(); ++it)
+	{
+		checkValidRoute(_header, *it, file_verif);
+		if (!file_verif.empty())
+		{
+			if (file_verif.at(file_verif.size() -1) != '/')
+			{
+				if (!(*it).getCgi().empty())
+				{
+					for(std::size_t i = 0; i < (*it).getCgi().size(); i++ )
+					{
+						for (std::size_t j = 0; j < (*it).getCgi()[i].extention.size(); j++)
+						{
+							if(file_verif.substr(file_verif.find_last_of(".")) == (*it).getCgi()[i].extention[j])
+								return (*it).getCgi()[i].path;
+						}
+					}
+				}
+			}
+		}
+	}
+	addLog("Not Path implemented for this extension")
+	return 0;
+} 
+
 std::string	TcpServer::true_path(std::vector<Route> _routes, HttpHeader _header)
 {
 	std::string file_verif;
@@ -28,60 +58,67 @@ std::string	TcpServer::true_path(std::vector<Route> _routes, HttpHeader _header)
 
 bool TcpServer::isCgi(std::vector<Route> route, HttpHeader &_header)
 {
-		std::string file_verif;
-		for (std::vector<Route>::iterator it = route.begin(); it != route.end(); ++it)
+	std::string file_verif;
+	for (std::vector<Route>::iterator it = route.begin(); it != route.end(); ++it)
+	{
+		checkValidRoute(_header, *it, file_verif);
+		if (!file_verif.empty())
 		{
-			checkValidRoute(_header, *it, file_verif);
-			if (!file_verif.empty())
+			if (file_verif.at(file_verif.size() -1) != '/')
 			{
-				if (file_verif.at(file_verif.size() -1) != '/')
+				if (!(*it).getCgi().empty())
 				{
-					if (!(*it).getCgi().empty())
+					for(std::size_t i = 0; i < (*it).getCgi().size(); i++ )
 					{
-						for(std::size_t i = 0; i < (*it).getCgi().size(); i++ )
+						for (std::size_t j = 0; j < (*it).getCgi()[i].extention.size(); j++)
 						{
-							for (std::size_t j = 0; j < (*it).getCgi()[i].extention.size(); j++)
-							{
-								if(file_verif.substr(file_verif.find_last_of(".")) == (*it).getCgi()[i].extention[j])
-									return true;
-							}
+							if(file_verif.substr(file_verif.find_last_of(".")) == (*it).getCgi()[i].extention[j])
+								return true;
 						}
-						return false;
 					}
-					else
-					{
-						addLog("CGI Not implemented");
-						return false;
-					}
+					return false;
+				}
+				else
+				{
+					addLog("CGI Not implemented");
+					return false;
 				}
 			}
 		}
-		return false;
+	}
+	addLog("Route CGI Not implemented");
+	return false;
 }
 
 
-std::string TcpServer::execCgi(HttpHeader _header, std::string true_path)
+void TcpServer::execCgi(HttpHeader _header, std::string true_path, std::vector<Route> routes)
 {
-	/*std::cout << std::endl << "Lapis Lazuli" << std::endl;
-	std::cout << std::endl << "Method : " << _header.getMethod() << std::endl;
-	for (std::map<std::string, std::string >::iterator it  = _header.getArgs().begin(); it != _header.getArgs().end(); ++it)
+	std::string answer;
+	std::string _path;
+
+	path = cgiPath(routes, _header);
+
+	
+	if (_header.getMethod() == "GET"  || _header.getMethod() == "POST") 
 	{
-		std::cout << it->first << " => " << it->second << std::endl;
-	}	
-	std::cout << "Fin Lapis Lazuli" << std::endl;
-	return ;*/
-	/*if (_header.getMethod() == "POST")
-	{
-		execCgiPost(_header);
+		answer = (execCgiGet(_header, true_path, _path));
 	}
-	else*/ if (_header.getMethod() == "GET")
+
+	if (!answer.empty() && !path == 0)
 	{
-		return (execCgiGet(_header, true_path));
+		answer.insert(0,  buildHeader(".html" , 200, awnser.size()));
+		send(_newSocket, awnser.c_str(), awnser.size(), 0);
 	}
-	return (0);
+	else
+	{
+		//page d'erreur;
+	}
+	
+	//envoyer une page d'erreur
+	//return (0);
 }
 
-std::string TcpServer::execCgiGet(HttpHeader _header, std::string true_path)
+std::string TcpServer::execCgiGet(HttpHeader _header, std::string true_path, std::string _path)
 {
 	int pipe_fd[2];
 	pid_t pid;
@@ -110,10 +147,10 @@ std::string TcpServer::execCgiGet(HttpHeader _header, std::string true_path)
 		
 		std::string file = true_path; //// faire un getteur pour la Root
 		char *script_path = const_cast<char *>(file.c_str());
-		char *argv[] = { script_path, NULL };
+		char *argv[] = { path, script_path, NULL };
 		char *envp[] = { NULL };
 
-		if (execve(script_path, argv, envp) == -1)
+		if (execve(path, argv, envp) == -1)
 			std::cerr << "execve failed" << std::endl;
 		exit(0);
 	}
