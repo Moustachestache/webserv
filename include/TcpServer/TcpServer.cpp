909d2490d97953fc5bdd083923e80a5952e9d9c0
gbricot <gbricot@student.42perpignan.fr>
2024-06-05 13:47:50 +0200

    Just adding as displayed path the index of the requested one logical.. 2 weeks for this..

--- include/TcpServer/TcpServer.cpp
+++ include/TcpServer/TcpServer.cpp
@@ -115,16 +115,13 @@ void	TcpServer::ServerAnswerLs(HttpHeader &header, std::string path)
 	std::string			output;
 	DIR					*openDir = opendir(path.c_str());
 
-	(void) header;
-	//path.erase(0, _root.size() + 1); // do not remove + 1 risk to delete one more char
-	path.erase(0, _root.size()); // nul we need the real path and the client one, to handle redirections
 	if (openDir == NULL)
 	{
 		ServerAnswerError(500);
 		return ;
 	}
 	output.append("<!DOCTYPE html><html data-theme=\"dark\"><head><link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css\"/><link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.colors.min.css\" /><link href=\"https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css\" rel=\"stylesheet\"><title>");
-	output.append(_serverName + "/" + path + " folder listing</title></head><body><div class=\"container\"><h1 class=\"grid\">index of " + path + "</h1><table><th></th><th>type</th><th>name</th><th>mime/type</th>");
+	output.append(_serverName + header.getFile() + " folder listing</title></head><body><div class=\"container\"><h1 class=\"grid\">index of " + header.getFile() + "</h1><table><th></th><th>type</th><th>name</th><th>mime/type</th>");
 	for (dirent	*folderScan = readdir(openDir); openDir != NULL && folderScan != NULL; folderScan = readdir(openDir))
 	{
 		output.append("<tr class=\"pico-background-grey-850\">");
